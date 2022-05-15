bc227833d7c6979d24f54de5641a0bccc15cc0fc
Nowshad Ruhani <nowshad.cse@gmail.com>
2022-05-15 19:25:37 +0600

    code level update

--- myfirstapp/lib/main.dart
+++ myfirstapp/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Flutter First app",
+        title: "My Flutter First App",
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
--- myfirstapp/lib/screens/register_screen.dart
+++ myfirstapp/lib/screens/register_screen.dart
@@ -7,7 +7,7 @@ class RegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Register"),
+        title: Text("Register Your Acoount"),
         centerTitle: true,
         backgroundColor: Colors.orangeAccent,
       ),
